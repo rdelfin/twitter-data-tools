eaee5c45cdb92294acd97ff5ea72ddcdc247b1c7
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-09-08 18:30:08 -0500

    Added tweetlist.py

---
+++ data/tweetlist.py
@@ -0,0 +1,28 @@
+import csv
+import nltk
+
+def tweetIndexToSent(dictionary, tweets):
+    return [[dictionary[word] for word in tweet] for tweet in tweets]
+
+def loadTweetCsv(fileDir):
+   tweetFile = open(fileDir, 'r')
+   tweetReader = csv.reader(tweetFile, delimiter=',', quotechar='"')
+   result = [[int(index) for index in tweet] for tweet in tweetReader]
+   tweetFile.close()
+   return result
+
+def loadDictionary(fileDir):
+    dictionaryFile = open(fileDir, 'r')
+    lines = dictionaryFile.readlines();
+    dictionaryFile.close()
+    return [line.rstrip('\n') for line in lines]
+
+def getTweets(dictionaryFile, tweetFile):
+    tweetIdx = loadTweetCsv(tweetFile)
+    dictionary = loadDictionary(dictionaryFile)
+    return tweetIndexToSent(dictionary, tweetIdx)
+
+def tweetBigramDist(tweets):
+    sentenceBigrams = [list(nltk.bigrams(tweet)) for tweet in tweets]
+    tweetBigrams = [bigram from bigram in sentence
+    tweetBigrams = nltk.bigrams(
